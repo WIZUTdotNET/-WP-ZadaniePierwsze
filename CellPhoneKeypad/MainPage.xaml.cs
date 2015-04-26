5b5889bac08f77fc697b61775d1f5a21a64949b5
Patryk Matuszak <patryk.mtk@gmail.com>
2015-04-26 16:20:26 +0200

    Added MainPage.xaml.cs and assets to project

---
+++ CellPhoneKeypad/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using CellPhoneKeypad.Resources;
+
+namespace CellPhoneKeypad
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
