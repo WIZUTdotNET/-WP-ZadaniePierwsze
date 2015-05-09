3186fa4f85a511e23ffe6fe0dafda786dc972db6
bobista0 <borys.rybak@gmail.com>
2015-05-09 18:42:49 +0200

    Added solution

--- CellPhoneKeypad/MainPage.xaml.cs
+++ CellPhoneKeypad/MainPage.xaml.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using CellPhoneKeypad.Resources;
+using Microsoft.Xna.Framework.Media;
 
 namespace CellPhoneKeypad
 {
@@ -17,6 +22,43 @@ namespace CellPhoneKeypad
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void KeypadButtonManipulationStarted(object sender, RoutedEventArgs e)
+        {
+            Button currentButton = sender as Button;
+
+            if (currentButton == null) return;
+
+            String buttonContentValue = currentButton.Content as String;
+
+            if (buttonContentValue == null) return;
+
+            PlayTargetKeypadSound(buttonContentValue);
+            RefreshNumber(buttonContentValue);
+        }
+
+        private void PlayTargetKeypadSound(string buttonContentValue)
+        {
+            if (buttonContentValue == "*")
+                buttonContentValue = "star";
+            if (buttonContentValue == "#")
+                buttonContentValue = "hash";
+
+            String path = "/Assets/Sounds/keyNumber_" + buttonContentValue + ".mp3";
+            KeySoundMediaElement.MediaOpened += KeySoundMediaElementOnMediaOpened;
+            KeySoundMediaElement.Source = new Uri(path, UriKind.RelativeOrAbsolute);
+        }
+
+        private void KeySoundMediaElementOnMediaOpened(object sender, RoutedEventArgs routedEventArgs)
+        {
+            MediaElement sound = sender as MediaElement;
+            if (sound != null) sound.Play();
+        }
+
+        private void RefreshNumber(string buttonContentValue)
+        {
+            NumberTextBlock.Text = buttonContentValue;
         }
     }
 }
